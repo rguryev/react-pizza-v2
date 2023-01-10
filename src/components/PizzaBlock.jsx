8a51d6890c65b8069bb1cf6ed21706f32a09f4ce
rguryev <rgguryev@gmail.com>
2023-01-10 23:43:50 +0100

    Lesson_5

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -14,6 +14,7 @@ const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
 				<ul>
 					{types.map(typeId => (
 						<li
+							key={typeId}
 							onClick={() => setActiveType(typeId)}
 							className={activeType === typeId ? 'active' : ''}>
 							{typeNames[typeId]}
@@ -22,7 +23,10 @@ const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
 				</ul>
 				<ul>
 					{sizes.map((size, i) => (
-						<li onClick={() => setActiveSize(i)} className={i === activeSize ? 'active' : ''}>
+						<li
+							key={size}
+							onClick={() => setActiveSize(i)}
+							className={i === activeSize ? 'active' : ''}>
 							{size} см.
 						</li>
 					))}
