dded8a634ada6df4fbef517ebcb7ffb0dc9d5f20
rguryev <rgguryev@gmail.com>
2023-01-16 22:06:38 +0100

    Lesson_6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
 
-import pizzas from './assets/pizzas.json';
-
 function App() {
+	const [items, setItems] = React.useState([]);
+
+	React.useEffect(() => {
+		try {
+			fetch('https://63c5ae74f80fabd877ee1f3f.mockapi.io/items')
+				.then(res => res.json())
+				.then(json => {
+					setItems(json);
+				});
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
+
 	return (
 		<div className='wrapper'>
 			<Header />
@@ -18,7 +31,7 @@ function App() {
 					</div>
 					<h2 className='content__title'>Все пиццы</h2>
 					<div className='content__items'>
-						{pizzas.map(obj => (
+						{items.map(obj => (
 							<PizzaBlock key={obj.id} {...obj} />
 						))}
 					</div>
